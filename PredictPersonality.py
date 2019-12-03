7c2e81aca3548d1553b14b9f50587b0e2f291aa1
Dallin Clayton <dallin.clayton@gmail.com>
2019-12-03 10:56:19 -0700

    Makin a bar graph

--- PredictPersonality.py
+++ PredictPersonality.py
@@ -2,108 +2,113 @@ from gen_data import *
 import numpy as np
 from scipy.optimize import leastsq, least_squares
 from tqdm import tqdm
+def test_leastsq():
+    #CONSTS
+    #######
+    #amount of pages
+    M = 40
+    PAGE_SCORES = gen_ocean_score(M)
+    #numher of pages each person will like
+    PAGE_LIKE_COUNT = 5
 
-#CONSTS
-#######
+    #training Data
+    ##############
+    n_train = 10000
+    train_people_scores = gen_ocean_score(n_train)
+    likes_train = np.zeros((n_train, 5))
+    train_people_likes = [gen_page_likes(train_people_scores[i], PAGE_SCORES, PAGE_LIKE_COUNT) for i in range(n_train)]
 
-#amount of pages
-M = 40
-PAGE_SCORES = gen_ocean_score(M)
-#numher of pages each person will like
-PAGE_LIKE_COUNT = 5
+    #testing data
+    #############
+    n_test = 200
+    test_people_scores = gen_ocean_score(n_test)
+    test_people_likes = [gen_page_likes(test_people_scores[i], PAGE_SCORES, PAGE_LIKE_COUNT) for i in range(n_test)]
 
-#training Data
-##############
-n_train = 3000
-train_people_scores = gen_ocean_score(n_train)
-likes_train = np.zeros((n_train, 5))
-train_people_likes = [gen_page_likes(train_people_scores[i], PAGE_SCORES, PAGE_LIKE_COUNT) for i in range(n_train)]
+    #train model
+    ############
+    person_scores_of_liked_pages = {}
 
-#testing data
-#############
-n_test = 200
-test_people_scores = gen_ocean_score(n_test)
-test_people_likes = [gen_page_likes(test_people_scores[i], PAGE_SCORES, PAGE_LIKE_COUNT) for i in range(n_test)]
+    #Predict page scores by taking the mean personality score of everyone who liked each page
+    print("Predicting page scores bassed on training data")
+    for i in tqdm(range(n_train)):
+        person_score = train_people_scores[i].copy()
+        person_likes, = train_people_likes[i]
+        for page_idx in person_likes:
+            if page_idx in person_scores_of_liked_pages:
+                person_scores_of_liked_pages[page_idx] = np.vstack([
+                    person_scores_of_liked_pages[page_idx],
+                    person_score
+                ])
+            else:
+                person_scores_of_liked_pages[page_idx] = person_score
 
-#train model
-############
-person_scores_of_liked_pages = {}
+    pred_page_scores = [np.mean(person_scores_of_liked_pages[i], axis=0) for i in range(M)]
 
-#Predict page scores by taking the mean personality score of everyone who liked each page
-print("Predicting page scores bassed on training data")
-for i in tqdm(range(n_train)):
-    person_score = train_people_scores[i].copy()
-    person_likes, = train_people_likes[i]
-    for page_idx in person_likes:
-        if page_idx in person_scores_of_liked_pages:
-            person_scores_of_liked_pages[page_idx] = np.vstack([
-                person_scores_of_liked_pages[page_idx],
-                person_score
-            ])
-        else:
-            person_scores_of_liked_pages[page_idx] = person_score
+    # TODO okay so we are going to try the information geometry methods (look up InformationGeometry-Exp.ipynb)
+    # r stands for residual or error, but its not right
+    #   Variance for a uniform random distribution on (a,b) is (b-a)^2 / 12, but do we have to factor in the gaussian variance as well? Well since its the identity, and I think you just invert the covariance, it wouldn't change the answer
+    #   lstsq takes in a vector to fit
+    def r(x, unkown_likes):
+        # TODO penalize really bad x values somehow
+        # Attempting to penalize crazy x values
+        if np.max(np.abs(x)) > 1.0:
+            return np.ones(x.shape) * np.exp(np.max(np.abs(x)))
+        unkown_like_ones = np.zeros(PAGE_SCORES.shape[0])
+        unkown_like_ones[unkown_likes] = 1
+        # Try some monte carlo
+        input_mc = []
+        for _ in range(50):
+            input_like_ones = np.zeros(PAGE_SCORES.shape[0])
+            input_like_ones[gen_page_likes(x, PAGE_SCORES, PAGE_LIKE_COUNT, include_noise=False)] = 1
+            input_mc.append(input_like_ones)
+        input_mc = np.array(input_mc)
+        conglomerant = np.mean(input_mc, axis=0)
+        conglomerant[conglomerant > 0.5] = 1.0
+        conglomerant[conglomerant < 0.5] = 0.0
+        # TODO TODO figure out how to find the difference correctly for this method, nothing seems to be working
+        #   Um I think the way I'm using this its supposed to be the differences of each one
+        # result = np.power(np.abs(np.power(unkown_like_ones, 2) - np.power(conglomerant, 2)), 0.5)
+        # return result / 3.0
+        # return np.linalg.norm(unkown_like_ones - conglomerant, ord=2) / 3.0
+        # return np.linalg.norm(unkown_like_ones - conglomerant, ord=2)
+        return unkown_like_ones - conglomerant # this seems to work the best
+        # return (unkown_like_ones - input_like_ones) / 3.0
+        # return ((unkown_likes - gen_page_likes(x, PAGE_SCORES, PAGE_LIKE_COUNT, include_noise=False))/3.0).reshape(5)
 
-pred_page_scores = [np.mean(person_scores_of_liked_pages[i], axis=0) for i in range(M)]
+    #Predict person scores by taking the average of all the *predicted* page scores they liked
+    print("Predicting test data scores")
+    pred_test_people_scores = np.zeros((n_test, 5))
+    for i in tqdm(range(n_test)):
+        page_likes, = test_people_likes[i]
+        page_likes_matrix = np.zeros((len(page_likes), 5))
+        for j, page_idx in enumerate(page_likes):
+            page_likes_matrix[j, :] = pred_page_scores[page_idx]
 
-# TODO okay so we are going to try the information geometry methods (look up InformationGeometry-Exp.ipynb)
-# r stands for residual or error, but its not right
-#   Variance for a uniform random distribution on (a,b) is (b-a)^2 / 12, but do we have to factor in the gaussian variance as well? Well since its the identity, and I think you just invert the covariance, it wouldn't change the answer
-#   lstsq takes in a vector to fit
-def r(x, unkown_likes):
-    # TODO penalize really bad x values somehow
-    # Attempting to penalize crazy x values
-    if np.max(np.abs(x)) > 1.0:
-        return np.ones(x.shape) * np.exp(np.max(np.abs(x)))
-    unkown_like_ones = np.zeros(PAGE_SCORES.shape[0])
-    unkown_like_ones[unkown_likes] = 1
-    # Try some monte carlo
-    input_mc = []
-    for _ in range(50):
-        input_like_ones = np.zeros(PAGE_SCORES.shape[0])
-        input_like_ones[gen_page_likes(x, PAGE_SCORES, PAGE_LIKE_COUNT, include_noise=False)] = 1
-        input_mc.append(input_like_ones)
-    input_mc = np.array(input_mc)
-    conglomerant = np.mean(input_mc, axis=0)
-    conglomerant[conglomerant > 0.5] = 1.0
-    conglomerant[conglomerant < 0.5] = 0.0
-    # TODO TODO figure out how to find the difference correctly for this method, nothing seems to be working
-    #   Um I think the way I'm using this its supposed to be the differences of each one
-    # result = np.power(np.abs(np.power(unkown_like_ones, 2) - np.power(conglomerant, 2)), 0.5)
-    # return result / 3.0
-    # return np.linalg.norm(unkown_like_ones - conglomerant, ord=2) / 3.0
-    # return np.linalg.norm(unkown_like_ones - conglomerant, ord=2)
-    return unkown_like_ones - conglomerant # this seems to work the best
-    # return (unkown_like_ones - input_like_ones) / 3.0
-    # return ((unkown_likes - gen_page_likes(x, PAGE_SCORES, PAGE_LIKE_COUNT, include_noise=False))/3.0).reshape(5)
+        # pred_test_people_scores[i, :] = np.mean(page_likes_matrix, axis=0)
 
-#Predict person scores by taking the average of all the *predicted* page scores they liked
-print("Predicting test data scores")
-pred_test_people_scores = np.zeros((n_test, 5))
-for i in tqdm(range(n_test)):
-    page_likes, = test_people_likes[i]
-    page_likes_matrix = np.zeros((len(page_likes), 5))
-    for j, page_idx in enumerate(page_likes):
-        page_likes_matrix[j, :] = pred_page_scores[page_idx]
+        pred_score = np.mean(page_likes_matrix, axis=0)
+        # TODO scipy also has a least_squares function, maybe try using that?
+        final_pred, msg = leastsq(r, pred_score, args=(page_likes))
+        # final_pred, msg = least_squares(r, pred_score, args=(page_likes))
+        # final_pred, msg = leastsq(r, gen_ocean_score(), args=(page_likes))
+        pred_test_people_scores[i, :] = final_pred
+        
+    # print("Using just mean of predicted page scores")
+    print("Using leastsq to try to fit better")
 
-    # pred_test_people_scores[i, :] = np.mean(page_likes_matrix, axis=0)
-
-    pred_score = np.mean(page_likes_matrix, axis=0)
-    # TODO scipy also has a least_squares function, maybe try using that?
-    final_pred, msg = leastsq(r, pred_score, args=(page_likes))
-    # final_pred, msg = least_squares(r, pred_score, args=(page_likes))
-    # final_pred, msg = leastsq(r, gen_ocean_score(), args=(page_likes))
-    pred_test_people_scores[i, :] = final_pred
-    
-# print("Using just mean of predicted page scores")
-print("Using leastsq to try to fit better")
-
-#test model
-###########
-print("MSE: ",
-        np.mean(
-            np.linalg.norm(pred_test_people_scores - test_people_scores, axis=1)
+    #test model
+    ###########
+    print("MSE: ",
+            np.mean(
+                np.linalg.norm(pred_test_people_scores - test_people_scores, axis=1)
+            )
         )
-    )
+    return np.mean( np.linalg.norm(pred_test_people_scores - test_people_scores, axis=1))
+
+leastsq_mean = test_leastsq()
+for i in range(9):
+    leastsq_mean = np.vstack([leastsq_mean, test_leastsq()])
+print(np.mean(leastsq_mean, axis=0))
 # from itertools import permutations 
 # from sklearn import tree    
 # from sklearn.neighbors import KNeighborsClassifier
--- learning_harness.py
+++ learning_harness.py
@@ -1,101 +1,107 @@
 import gen_data as gd
 import numpy as np
 
-# generate p pages
+def method():
+    # generate p pages
 
-p = 40
-print("Number of pages: {}".format(p))
-pages = gd.gen_ocean_score(p)
+    p = 40
+    print("Number of pages: {}".format(p))
+    pages = gd.gen_ocean_score(p)
 
-# generate x people who's scores are known
-x = 3000
-print("Number of known scores: {}".format(x))
-known_people = gd.gen_ocean_score(x)
-# TODO should we do the same thing with other functions allowing it to return a multidimensional array?
-num_likes = 5
-print("Number of likes per person: {}".format(num_likes))
-known_likes = gd.gen_page_likes(known_people, pages, num_likes)
-known_likes_ones = []
-# convert likes into a ones vector
-for like in known_likes:
-    ones = np.zeros(pages.shape[0])
-    ones[like] = 1
-    known_likes_ones.append(ones)
-known_likes_ones = np.array(known_likes_ones)
+    # generate x people who's scores are known
+    x = 10000
+    print("Number of known scores: {}".format(x))
+    known_people = gd.gen_ocean_score(x)
+    # TODO should we do the same thing with other functions allowing it to return a multidimensional array?
+    num_likes = 5
+    print("Number of likes per person: {}".format(num_likes))
+    known_likes = gd.gen_page_likes(known_people, pages, num_likes)
+    known_likes_ones = []
+    # convert likes into a ones vector
+    for like in known_likes:
+        ones = np.zeros(pages.shape[0])
+        ones[like] = 1
+        known_likes_ones.append(ones)
+    known_likes_ones = np.array(known_likes_ones)
 
-# generate y unknown people
-y = 200
-print("Number of unknown scores: {}".format(y))
-unknown_people = gd.gen_ocean_score(y)
-unknown_likes = gd.gen_page_likes(unknown_people, pages, num_likes)
+    # generate y unknown people
+    y = 200
+    print("Number of unknown scores: {}".format(y))
+    unknown_people = gd.gen_ocean_score(y)
+    unknown_likes = gd.gen_page_likes(unknown_people, pages, num_likes)
 
-# print("pages")
-# print(pages)
-# print("known_people")
-# print(known_people)
-# print("known_likes")
-# print(known_likes)
+    # print("pages")
+    # print(pages)
+    # print("known_people")
+    # print(known_people)
+    # print("known_likes")
+    # print(known_likes)
 
-# now try to cluster a person
-#   first idea: just average of known people's scores weighted by their distance from those people's page likes
-# AWESOME idea: use known people to intuit parameters for a multivariate gaussian for each page and use likelihood that the unknown person with a certain score fit that page to fit the parameters or do gradient descent or something.
-#   Second idea: just cluster them, and then take some sort of distance metric from clusters to figure out score probably...
-#   third idea: try to generate our own page likes for a person and use some sort of gradient descent or nonlinear regression
-#   fourth idea: maximum likelihood of gaussian that produced that page like combo??? is that legal? model can't know page ocean scores
-guessed_score = []
-two_norm_total = 0
-one_norm_total = 0
-for i, like in enumerate(unknown_likes):
-    # turn likes into a vector of zeros and ones
-    like_ones = np.zeros(pages.shape[0])
-    like_ones[like] = 1
-    # find the distance from each known person's likes
-    dists = []
-    for known_like in known_likes_ones:
-        dist = np.linalg.norm(known_like - like_ones, ord=1)
-        dists.append(dist)
-    dists = np.array(dists)
-    # do a weighted average of each of their ocean scores
-    estimate = np.zeros(5)
-    # TODO figure out why you are getting a 0 in the denominator (it has something to do with the unknown person having the same likes as the known person)
-    for j, dist in enumerate(dists):
-        # print("{} / {} = {}".format(dist, np.sum(dists), dist / np.sum(dists)))
-        # print(known_likes[j])
-        # print(unknown_likes[i])
-        # TODO figure out what to return if this happens
-        if np.sum(dists) == 0:
-            estimate += known_people[j] * 1
-        else:
-            estimate += known_people[j] * (dist / np.sum(dists))
-    # see how close it is to the actual one
-    # print("Guess vs actual value")
-    # print(estimate)
-    # print(unknown_people[i])
-    # print("one and two norms of error")
-    # print(np.linalg.norm(estimate - unknown_people[i], ord=1))
-    # print(np.linalg.norm(estimate - unknown_people[i], ord=2))
-    one_norm_total += np.linalg.norm(estimate - unknown_people[i], ord=1)
-    two_norm_total += np.linalg.norm(estimate - unknown_people[i], ord=2)
+    # now try to cluster a person
+    #   first idea: just average of known people's scores weighted by their distance from those people's page likes
+    # AWESOME idea: use known people to intuit parameters for a multivariate gaussian for each page and use likelihood that the unknown person with a certain score fit that page to fit the parameters or do gradient descent or something.
+    #   Second idea: just cluster them, and then take some sort of distance metric from clusters to figure out score probably...
+    #   third idea: try to generate our own page likes for a person and use some sort of gradient descent or nonlinear regression
+    #   fourth idea: maximum likelihood of gaussian that produced that page like combo??? is that legal? model can't know page ocean scores
+    guessed_score = []
+    two_norm_total = 0
+    one_norm_total = 0
+    for i, like in enumerate(unknown_likes):
+        # turn likes into a vector of zeros and ones
+        like_ones = np.zeros(pages.shape[0])
+        like_ones[like] = 1
+        # find the distance from each known person's likes
+        dists = []
+        for known_like in known_likes_ones:
+            dist = np.linalg.norm(known_like - like_ones, ord=1)
+            dists.append(dist)
+        dists = np.array(dists)
+        # do a weighted average of each of their ocean scores
+        estimate = np.zeros(5)
+        # TODO figure out why you are getting a 0 in the denominator (it has something to do with the unknown person having the same likes as the known person)
+        for j, dist in enumerate(dists):
+            # print("{} / {} = {}".format(dist, np.sum(dists), dist / np.sum(dists)))
+            # print(known_likes[j])
+            # print(unknown_likes[i])
+            # TODO figure out what to return if this happens
+            if np.sum(dists) == 0:
+                estimate += known_people[j] * 1
+            else:
+                estimate += known_people[j] * (dist / np.sum(dists))
+        # see how close it is to the actual one
+        # print("Guess vs actual value")
+        # print(estimate)
+        # print(unknown_people[i])
+        # print("one and two norms of error")
+        # print(np.linalg.norm(estimate - unknown_people[i], ord=1))
+        # print(np.linalg.norm(estimate - unknown_people[i], ord=2))
+        one_norm_total += np.linalg.norm(estimate - unknown_people[i], ord=1)
+        two_norm_total += np.linalg.norm(estimate - unknown_people[i], ord=2)
 
-# print("Average one norm: {}".format(one_norm_total / y))
-print("Average two norm: {}".format(two_norm_total / y))
-print("-----------------------------------------------------")
+    # print("Average one norm: {}".format(one_norm_total / y))
+    print("Average two norm: {}".format(two_norm_total / y))
+    print("-----------------------------------------------------")
 
-# Now to try just uniform random guessing
-baseline_two_norm_total = 0
-one_norm_total = 0
-for i, like in enumerate(unknown_likes):
-    baseline_estimate = gd.gen_ocean_score()[0]
-    # print("Guess vs actual value")
-    # print(baseline_estimate)
-    # print(unknown_people[i])
-    # print("one and two norms of error")
-    # print(np.linalg.norm(estimate - unknown_people[i], ord=1))
-    # print(np.linalg.norm(estimate - unknown_people[i], ord=2))
-    one_norm_total += np.linalg.norm(baseline_estimate - unknown_people[i], ord=1)
-    baseline_two_norm_total += np.linalg.norm(baseline_estimate - unknown_people[i], ord=2)
+    # # Now to try just uniform random guessing
+    # baseline_two_norm_total = 0
+    # one_norm_total = 0
+    # for i, like in enumerate(unknown_likes):
+    #     baseline_estimate = gd.gen_ocean_score()[0]
+    #     # print("Guess vs actual value")
+    #     # print(baseline_estimate)
+    #     # print(unknown_people[i])
+    #     # print("one and two norms of error")
+    #     # print(np.linalg.norm(estimate - unknown_people[i], ord=1))
+    #     # print(np.linalg.norm(estimate - unknown_people[i], ord=2))
+    #     one_norm_total += np.linalg.norm(baseline_estimate - unknown_people[i], ord=1)
+    #     baseline_two_norm_total += np.linalg.norm(baseline_estimate - unknown_people[i], ord=2)
 
-# print("Average baseline one norm: {}".format(one_norm_total / y))
-print("Average baseline two norm: {}".format(baseline_two_norm_total / y))
-baseline_two_norm_total - two_norm_total / baseline_two_norm_total
-print("ratio: {}".format( (baseline_two_norm_total - two_norm_total) / baseline_two_norm_total))
+    # # print("Average baseline one norm: {}".format(one_norm_total / y))
+    # print("Average baseline two norm: {}".format(baseline_two_norm_total / y))
+    # baseline_two_norm_total - two_norm_total / baseline_two_norm_total
+    # print("ratio: {}".format( (baseline_two_norm_total - two_norm_total) / baseline_two_norm_total))
+    return two_norm_total
+totals = method()
+for i in range(9):
+    totals = np.vstack([totals, method()])
+print(np.mean(totals, axis=0))
