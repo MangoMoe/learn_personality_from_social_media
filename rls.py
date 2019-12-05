1e58be2b7e76cf78cd51af1b0d0644357a0fbe57
Dallin Clayton <dallin.clayton@gmail.com>
2019-12-05 09:04:43 -0700

    Made it into a function for easy experiment

--- rls.py
+++ rls.py
@@ -10,98 +10,103 @@ from tqdm import tqdm
 #   just stack the ocean scores of the output pages (so basically the page ocean scores are known) vertically because the similarity is the same for the different ocean parameters of each person
 # TODO document what you reused
 
-NUM_KNOWN_PEOPLE = 300
-NUM_PAGES = 40
-LIKES_PER_PERSON = 10
+def rls_fit(num_known_people, num_pages, likes_per_person, window, num_iter, hide_page_scores=True):
 
-WINDOW = 30
-NUM_ITER = 500
-DATA_SIZE = NUM_PAGES
-# DATA_SIZE = 5
-pages = gd.gen_ocean_score(NUM_PAGES)
-# TODO merge other code into this so we have the updated page likes generation function probably...
-known_people = gd.gen_ocean_score(NUM_KNOWN_PEOPLE)
-unknown_person = gd.gen_ocean_score()
-params = np.zeros((1, NUM_KNOWN_PEOPLE)).reshape((1,NUM_KNOWN_PEOPLE)).T
+    NUM_KNOWN_PEOPLE = num_known_people
+    NUM_PAGES = num_pages
+    LIKES_PER_PERSON = likes_per_person if hide_page_scores else 1
 
-# TODO this could end up being too hard, you might have to use the same draw to produce all the vectors
-def gen_likes_matricies(known_people, pages, unknown_person):
-    # Return A matrix (well the additional part anyway) and the b vector (additional part)
-    
-    stuff = []
-    for person in known_people:
-        one_vec = np.zeros(pages.shape[0])
-        one_vec[gd.gen_page_likes(person, pages, LIKES_PER_PERSON)] = 1
-        stuff.append(one_vec.reshape(1, DATA_SIZE))
-
-    A = np.concatenate(stuff)
-    # A = np.concatenate([pages[gd.gen_page_likes(person, pages, LIKES_PER_PERSON)].reshape(LIKES_PER_PERSON,DATA_SIZE) for person in known_people])
-    
-    b = np.zeros(pages.shape[0])
-    b[gd.gen_page_likes(unknown_person, pages, LIKES_PER_PERSON)] = 1
-    b = b.reshape(1,DATA_SIZE)
-    return A.T, b.T
-
-A, b = gen_likes_matricies(known_people, pages, unknown_person)
-# new_A, new_b = gen_likes_matricies(known_people, pages, unknown_person)
-# A = np.vstack([ A, new_A ])
-# b = np.vstack([ b, new_b ])
-
-# P = R^-1, R = A^H A
-# f is input signal, which is probably A or in other words, our stream of data from the other people
-# d is data or the signal we are trying to follow, so b
-# TODO don't calculate the inverse directly?
-# initialize
-t = 0
-delta = 0.001
-# d = [b]
-fs = [A]
-data = [b]
-k = 0 # initial value for K is empty
-# TODO do we need to store the previous P matricies? probably not, also probably not k
-# P = (1/delta) * np.identity(5 * WINDOW)
-P = (1/delta) * np.identity(NUM_KNOWN_PEOPLE)
-h = params
-q_init = np.zeros((DATA_SIZE * WINDOW, NUM_KNOWN_PEOPLE))
-d_init = np.zeros((DATA_SIZE * WINDOW, 1))
-for t in tqdm(range(1,NUM_ITER+1)):
-    A, b = gen_likes_matricies(known_people, pages, unknown_person)
-    fs.append(A)
-    data.append(b)
-    if t < WINDOW + 1:
-        q = np.zeros(q_init.shape)
-        for i in range(t):
-            q[i *DATA_SIZE: i*DATA_SIZE+DATA_SIZE,:] = fs[t - i]
-        d = np.zeros(d_init.shape)
-        for i in range(t):
-            d[i *DATA_SIZE: i*DATA_SIZE+DATA_SIZE,:] = data[t - i]
+    WINDOW = window
+    NUM_ITER = num_iter
+    if hide_page_scores:
+        DATA_SIZE = NUM_PAGES # use those one vectors composed of which pages they liked
     else:
-        q = np.vstack(fs[t:t - WINDOW + 1 - 1:-1])
-        d = np.vstack(data[t:t - WINDOW + 1 - 1:-1])
-    q = q.T # apparently this worked... TODO make sure this is the case
+        DATA_SIZE = 5 # length of an ocean score
+    print("Running RLS Algorithm.\n Number of known people is {},\n Number of pages is {},\n Likes per person is {},\n window size is {} sets of data,\n we are iterating {} times,\n and are we hiding the page scores? {}".format(
+        NUM_KNOWN_PEOPLE, NUM_PAGES, LIKES_PER_PERSON, WINDOW, NUM_ITER, "Yes" if hide_page_scores else "No"
+    ))
+    pages = gd.gen_ocean_score(NUM_PAGES)
+    known_people = gd.gen_ocean_score(NUM_KNOWN_PEOPLE)
+    unknown_person = gd.gen_ocean_score()
+    params = np.zeros((1, NUM_KNOWN_PEOPLE)).reshape((1,NUM_KNOWN_PEOPLE)).T
 
-    k = P@q@np.linalg.inv(np.identity(DATA_SIZE * WINDOW) + q.T@P@q) # TODO I used identity instead of one...
-    P = P - k@q.T@P
-    h = h + k@(d - q.T@h)
+    def gen_likes_matricies(known_people, pages, unknown_person, hide_page_scores=True):
+        # Return A matrix (well the additional part anyway) and the b vector (additional part)
+        if hide_page_scores:
+            stuff = []
+            for person in known_people:
+                one_vec = np.zeros(pages.shape[0])
+                one_vec[gd.gen_page_likes(person, pages, LIKES_PER_PERSON)] = 1
+                stuff.append(one_vec.reshape(1, DATA_SIZE))
 
-# at the end, predict the score of the person by combining the other people's scores
-# 
-h = h / np.linalg.norm(h, ord=1)
-# h = h *  DATA_SIZE / 5
-# h = h * 2
-estimated_score = (known_people.T@h).T
-baseline = gd.gen_ocean_score()
+            A = np.concatenate(stuff)
 
-print("End parameters: {}".format(h))
-print("Estimated person score: {}".format(estimated_score))
-print("Actual Person score: {}".format(unknown_person))
-# TODO TODO TODO compare to just mean of page scores for person likes
-#   even if it does work, its useful in other contexts where you are comparing people to see if they are similar
-# TODO try scaling answer to reduce error and figure out what optimal scaling is
-# print("Error: {}".format(estimated_score - unknown_person))
-print("Norm of error: {}".format(np.linalg.norm(estimated_score - unknown_person, ord=2)))
-# print("Baseline person score: {}".format(baseline))
-print("Norm of baseline error: {}".format(np.linalg.norm(baseline - unknown_person, ord=2)))
+            b = np.zeros(pages.shape[0])
+            b[gd.gen_page_likes(unknown_person, pages, LIKES_PER_PERSON)] = 1
+            b = b.reshape(1,DATA_SIZE)
+        else:
+            A = np.concatenate([pages[gd.gen_page_likes(person, pages, LIKES_PER_PERSON)].reshape(LIKES_PER_PERSON,DATA_SIZE) for person in known_people])
+            b = pages[gd.gen_page_likes(unknown_person, pages, LIKES_PER_PERSON)].reshape(1,DATA_SIZE)
+            
+        return A.T, b.T
 
+    A, b = gen_likes_matricies(known_people, pages, unknown_person, hide_page_scores)
+
+    # P = R^-1, R = A^H A
+    # f is input signal, which is probably A or in other words, our stream of data from the other people
+    # d is data or the signal we are trying to follow, so b
+    # TODO don't calculate the inverse directly?
+    # initialize
+    t = 0
+    delta = 0.001
+    fs = [A]
+    data = [b]
+    k = 0 # initial value for K is empty
+    # P = (1/delta) * np.identity(5 * WINDOW)
+    P = (1/delta) * np.identity(NUM_KNOWN_PEOPLE)
+    h = params
+    q_init = np.zeros((DATA_SIZE * WINDOW, NUM_KNOWN_PEOPLE))
+    d_init = np.zeros((DATA_SIZE * WINDOW, 1))
+    for t in tqdm(range(1,NUM_ITER+1)):
+        A, b = gen_likes_matricies(known_people, pages, unknown_person, hide_page_scores)
+        fs.append(A)
+        data.append(b)
+        if t < WINDOW + 1:
+            q = np.zeros(q_init.shape)
+            for i in range(t):
+                q[i *DATA_SIZE: i*DATA_SIZE+DATA_SIZE,:] = fs[t - i]
+            d = np.zeros(d_init.shape)
+            for i in range(t):
+                d[i *DATA_SIZE: i*DATA_SIZE+DATA_SIZE,:] = data[t - i]
+        else:
+            q = np.vstack(fs[t:t - WINDOW + 1 - 1:-1])
+            d = np.vstack(data[t:t - WINDOW + 1 - 1:-1])
+        q = q.T # apparently this worked... TODO make sure this is the case
+
+        k = P@q@np.linalg.inv(np.identity(DATA_SIZE * WINDOW) + q.T@P@q) # I used identity instead of one...
+        P = P - k@q.T@P
+        h = h + k@(d - q.T@h)
+
+    # at the end, predict the score of the person by combining the other people's scores
+    if hide_page_scores:
+        # TODO trying to figure out correct scaling for h when transitioning from descretized page likes to ocean scores
+        h = h / np.linalg.norm(h, ord=1)
+        # h = h *  DATA_SIZE / 5
+        # h = h * 2
+    estimated_score = (known_people.T@h).T
+    baseline = gd.gen_ocean_score()
+
+    # print("End parameters: {}".format(h))
+    print("Estimated person score: {}".format(estimated_score))
+    print("Actual Person score: {}".format(unknown_person))
+    # TODO TODO TODO compare to just mean of page scores for person likes
+    #   even if it does work, its useful in other contexts where you are comparing people to see if they are similar
+    # print("Error: {}".format(estimated_score - unknown_person))
+    print("Norm of error: {}".format(np.linalg.norm(estimated_score - unknown_person, ord=2)))
+    # print("Baseline person score: {}".format(baseline))
+    print("Norm of baseline error: {}".format(np.linalg.norm(baseline - unknown_person, ord=2)))
+    return np.linalg.norm(estimated_score - unknown_person, ord=2)
+
+rls_fit(num_known_people = 300, num_pages = 40, likes_per_person = 10, window = 10, num_iter = 1000, hide_page_scores = False)
 
 
